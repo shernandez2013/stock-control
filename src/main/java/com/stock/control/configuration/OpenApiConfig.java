c0ad4dd2d2c9cf68a9d9e3a438fc653ad72408e1
sihec <siheca2013@gmail.com>
2024-10-26 15:54:58 -0600

    CERT-0001 Add changes into table relation and change the update method for products
    added swagger configuration

--- src/main/java/com/stock/control/configuration/OpenApiConfig.java
+++ src/main/java/com/stock/control/configuration/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package com.stock.control.configuration;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
